5722684969e9f992529bce7aa535f2a352a80663
Glen <gelder@PSDevMac.local>
2015-04-12 16:52:41 -0700

    Created a constants file.

---
+++ Qix Game/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Qix Game
+//
+//  Created by Glen on 4/12/15.
+//  Copyright (c) 2015 Melodic Bug. All rights reserved.
+//
+
+public class Constants{
+    let playerCollisionBitMask:UInt32 = 0
+}
